ecdf297f898a884fdb04f43535ce0bd282ea9fa4
jin214930 <jin214930@naver.com>
2026-01-21 22:48:09 +0900

    fit: client paths 누락 수정

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,13 +1,13 @@
 import createClient from "openapi-fetch";
 import { useAuthStore } from "@/store/useAuthStore";
+import { paths } from "./schema";
 
-export const client = createClient({
+export const client = createClient<paths>({
     baseUrl: process.env.NEXT_PUBLIC_API_URL,
 });
 
 client.use({
     onRequest({ request }) {
-        // Zustand 스토어에서 최신 토큰 참조
         const token = useAuthStore.getState().accessToken;
         if (token) {
             request.headers.set("Authorization", `Bearer ${token}`);
